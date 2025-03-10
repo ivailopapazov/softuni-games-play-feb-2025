0476be281d14ce4c39fa72e9e66c1aff60e4b9a0
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-03-10 21:20:43 +0200

    Edit game functionallity

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -16,6 +16,9 @@ export default {
     create(gameData) {
         return request.post(baseUrl, gameData);
     },
+    edit(gameId, gameData) {
+        return request.put(`${baseUrl}/${gameId}`, { ...gameData, _id: gameId });
+    },
     delete(gameId) {
         return request.delete(`${baseUrl}/${gameId}`);
     },
