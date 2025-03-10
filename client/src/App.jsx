fe793717feefddceb3d2e4466e0f102a414ef217
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-03-10 19:02:16 +0200

    Extract some components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import Header from './components/header/Header'
+import Home from './components/home/Home'
 import './App.css'
 
 function App() {
-
     return (
         <div id="box">
+            <Header />
 
+            <main id="main-content">
+                <Home />
+            </main>
         </div>
     )
 }
