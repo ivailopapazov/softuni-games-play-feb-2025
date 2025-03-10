76b3e2b0e2665512996401a4b06ddfac46a2de69
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-03-10 21:06:22 +0200

    Delete game functionallity

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -16,4 +16,7 @@ export default {
     create(gameData) {
         return request.post(baseUrl, gameData);
     },
+    delete(gameId) {
+        return request.delete(`${baseUrl}/${gameId}`);
+    },
 };
