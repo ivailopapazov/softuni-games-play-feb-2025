1e228d625b21f118e6768da91b8834c97ca730f9
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-03-17 20:25:17 +0200

    Fix 204 no content response

--- client/src/utils/request.js
+++ client/src/utils/request.js
@@ -15,9 +15,15 @@ const request = async (method, url, data, options = {}) => {
     }
 
     const response = await fetch(url, options);
+    const responseContentType = response.headers.get('Content-Type');
+    if (!responseContentType) {
+        return;
+    }
+    
     const result = await response.json();
 
     return result;
+
 };
 
 export default {
