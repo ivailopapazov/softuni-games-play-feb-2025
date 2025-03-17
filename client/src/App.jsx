86b73e4836f7fb914c276a5eeab5bef807b2c0ba
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-03-17 19:43:15 +0200

    Use context for auth data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router'
 
+import { UserContext } from './contexts/UserContext'
+
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Login from './components/login/Login'
@@ -19,21 +21,23 @@ function App() {
     };
 
     return (
-        <div id="box">
-            <Header />
+        <UserContext.Provider value={{...authData, userLoginHandler}}>
+            <div id="box">
+                <Header />
 
-            <main id="main-content">
-                <Routes>
-                    <Route index element={<Home />} />
-                    <Route path="/games" element={<GameCatalog />} />
-                    <Route path="/games/create" element={<GameCreate />} />
-                    <Route path="/games/:gameId/details" element={<GameDetails email={authData.email} />} />
-                    <Route path="/games/:gameId/edit" element={<GameEdit />} />
-                    <Route path="/login" element={<Login onLogin={userLoginHandler} />} />
-                    <Route path="/register" element={<Register />} />
-                </Routes>
-            </main>
-        </div>
+                <main id="main-content">
+                    <Routes>
+                        <Route index element={<Home />} />
+                        <Route path="/games" element={<GameCatalog />} />
+                        <Route path="/games/create" element={<GameCreate />} />
+                        <Route path="/games/:gameId/details" element={<GameDetails />} />
+                        <Route path="/games/:gameId/edit" element={<GameEdit />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
+                </main>
+            </div>
+        </UserContext.Provider>
     )
 }
 
