2b14dc50daee11796c4c9d853dfa10de565ce77b
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-03-24 19:29:00 +0200

    Add user context consumer

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -2,15 +2,21 @@ import { Component } from 'react';
 import AdminComments from './admin-comments/AdminComments';
 
 import styles from './Admin.module.css';
+import { UserContext } from '../../contexts/UserContext';
 
 export default class Admin extends Component {
     render() {
         return (
-            <section className={styles['admin-section']}>
-                <h1>Admin Panel</h1>
+            <UserContext.Consumer>
+                {(context) => (
+                    <section className={styles['admin-section']}>
+                        <h1>Admin Panel</h1>
+                        <h2>Hello {context.email}</h2>
 
-                <AdminComments />
-            </section>
+                        <AdminComments />
+                    </section>
+                )}
+            </UserContext.Consumer>
         );
     }
 }
